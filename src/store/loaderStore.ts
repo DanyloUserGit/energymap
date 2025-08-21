26409db6f0b50e0993bdaee4dc47eddcc27c9420
danylo <danylom2007.m@gmail.com>
2025-08-21 07:40:48 +0300

    new data

--- src/store/loaderStore.ts
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-import type { LoaderState } from "../types";
-
-export const useLoaderStore = create<LoaderState>()((set) => ({
-  loading: false,
-  setLoading: (l: boolean) => set({ loading: l }),
-}));
